2bc138e4040fef627ae394fd20bcd8211038bf87
niezhiqiang <niezhiqiang0419@163.com>
2022-04-21 14:25:20 +0800

    test

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * @CreateTime: 2022/4/21 2:24 下午
+ * @Author: nnoobnie
+ * @Description: Main
+ */
+public class Main {
+}
--- src/com/nnoob/Test.java
+++
@@ -1,9 +0,0 @@
-package com.nnoob;
-
-/**
- * @CreateTime: 2022/4/21 2:14 下午
- * @Author: nnoobnie
- * @Description: Test
- */
-public class Test {
-}
